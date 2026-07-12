5ab87e7a3e854dd7373008b3d0d78956bad6a8a1
sigma-student-10 <mdafjolhossain20@gmail.com>
2026-07-12 11:52:01 +0600

    add new file

--- models/listing.js
+++ models/listing.js
@@ -7,18 +7,19 @@ const listingSchema = new Schema({
         required: true,
     },
     description: String,
-    image: {
-    filename: {
-        type: String,
-    },
-    url: {
-        type: String,
-        default: "https://stock.adobe.com/images/evening-view-from-a-hotel-balcony-overlooking-the-serene-sea/926892696",
-        set: (v) =>
-            v === ""
-                ? "https://stock.adobe.com/images/evening-view-from-a-hotel-balcony-overlooking-the-serene-sea/926892696"
-                : v,
-    },
+image: {
+  filename: {
+    type: String,
+  },
+  url: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        : v,
+  },
 },
     price: Number,
     location: String,
