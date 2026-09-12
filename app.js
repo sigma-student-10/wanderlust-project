0a947f0e6612a5f8a5fba2d4fc989632ba7eee5f
sigma-student-10 <mdafjolhossain20@gmail.com>
2026-09-12 22:33:08 +0600

    add new code

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -54,15 +55,15 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 //Create Route
-app.post("/listings", async (req, res, next) => {
-    try {
+app.post("/listings",
+     wrapAsync(async (req, res, next) => {
+    
         const newListing = new Listing(req.body.listing);
         await newListing.save();
         res.redirect("/listings");
-    } catch(err) {
-        next(err);
-    }   
-});
+   
+    })   
+);
 
 //Edit Route
 app.get("/listings/:id/edit", async (req, res) => {
