64e1865035d577a5e1196e2a3f5515c5f74aeda5
sigma-student-10 <mdafjolhossain20@gmail.com>
2026-09-23 13:53:04 +0600

    add new code

--- classroom/server.js
+++ classroom/server.js
@@ -3,23 +3,43 @@ const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
 const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 const sessionOptions = {
-    secret: "mysupersecretestring",
-    resave: false,
-    saveUninitialized: true,
+  secret: "mysupersecretestring",
+  resave: false,
+  saveUninitialized: true,
 };
 
 app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.successMsg = req.flash("success");
+    res.locals.errorMsg = req.flash("error");
+    next();
+});
 
 app.get("/register", (req, res) => {
-    let { name = "anonymous"} = req.query;
-    req.session.name = name;
-    res.redirect("/hello");
+  let { name = "anonymous" } = req.query;
+  req.session.name = name;
+
+  if(name === "anonymous"){
+    req.flash("error", "user not register");
+  } else {
+    req.flash("success", "user registered successfully!");
+  }
+  
+  
+  res.redirect("/hello");
 });
 
 app.get("/hello", (req, res) => {
-    res.send(`hello, ${req.session.name}`);
+    res.render("page.ejs", { name: req.session.name, });
 });
 
 // app.get("/reqcount", (req, res) => {
