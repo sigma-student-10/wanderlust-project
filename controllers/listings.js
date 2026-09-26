31968270200b2dbf350383e4d8b05fed94a89e1d
sigma-student-10 <mdafjolhossain20@gmail.com>
2026-09-26 20:13:07 +0600

    add new code

--- controllers/listings.js
+++ controllers/listings.js
@@ -28,25 +28,14 @@ module.exports.showListing = async (req, res) => {
     res.render("listings/show.ejs", { listing });
 };
 
-// module.exports.showListing = async (req, res) => {
-//    let { id } = req.params;
-//    const listing = await Listing.findById(id)
-//     .populate({path: "reviews",
-//        populate: {
-//         path: "author",
-//         }
-//       })
-//       .populate("owner");
-//     if (!listing) {
-//       req.flash("error", "Listing your requested for does not exist");
-//       return res.redirect("/listings");
-//     }
-//    res.render("listings/show.ejs", { listing });
-// };
 
 module.exports.createListing = async (req, res, next) => {
+    let url = req.file.path;
+    let filename = req.file.filename;
+    
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
+    newListing.image = {url, filename};
     await newListing.save();
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
--- routes/listing.js
+++ routes/listing.js
@@ -12,15 +12,12 @@ const upload = multer({storage});
 router
   .route("/")
   .get(wrapAsync(listingController.index))
-  // .post(
-  //   isLoggedIn,
-  //   validateListing,
-  //   wrapAsync(listingController.createListing),
-  // );
-
-  .post( upload.single('listing[image]'), (req, res) => {
-    res.send(req.file);
-  });
+  .post(
+    isLoggedIn,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.createListing)
+  );
 
 //New Route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
