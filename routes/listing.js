064ef1de384ba09c4feb8ae03e3a03980fd106b7
sigma-student-10 <mdafjolhossain20@gmail.com>
2026-09-26 19:40:42 +0600

    add new code

--- routes/listing.js
+++ routes/listing.js
@@ -6,7 +6,8 @@ const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
 const listingController = require("../controllers/listings.js");
 const multer = require("multer");
-const upload = multer({dest: 'uploads/'});
+const {storage} = require("../cloudConfig.js");
+const upload = multer({storage});
 
 router
   .route("/")
@@ -38,7 +39,7 @@ router
 //Edit Route
 router.get(
   "/:id/edit",
-  isLoggedIn,
+  isLoggedIn, 
   isOwner,
   wrapAsync(listingController.renderEditForm),
 );
